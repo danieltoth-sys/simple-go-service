d4ee67717501460700d574ee99792f15edb084e9
Daniel Toth <78477459+danieltoth-sys@users.noreply.github.com>
2022-09-11 09:39:29 +0200

    Added new func

--- main.go
+++ main.go
@@ -4,14 +4,26 @@ import (
 	"io"
 	"log"
 	"net/http"
+	"strconv"
 )
 
 func main() {
 	helloHandler := func(w http.ResponseWriter, req *http.Request) {
-		io.WriteString(w, "Hello, world!\n")
+		io.WriteString(w, otherMethod())
 	}
 
 	http.HandleFunc("/hello", helloHandler)
 	log.Println("Listing for requests at http://localhost:8000/hello")
 	log.Fatal(http.ListenAndServe(":8000", nil))
+}
+
+func otherMethod() string {
+	var a int = 15
+	var b int = 5
+
+	var c int = a + b
+
+	var result string = "Hello! A+B is: " + strconv.Itoa(c)
+
+	return result
 }
